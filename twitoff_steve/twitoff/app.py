53304297e54f33f68fddd0b96e89ef1cf55b84f9
Steven Schwab <schwabs@umich.edu>
2025-08-29 12:49:11 -0400

    add user and compare routes

--- twitoff_steve/twitoff/app.py
+++ twitoff_steve/twitoff/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User, Post
 from dotenv import load_dotenv
 import spacy
 from .x import add_or_update_user
+from .predict import predict_user
+
 
 def create_app():
     '''
@@ -32,32 +34,60 @@ def create_app():
         # indicated schema in models.py
         DB.create_all()
         return render_template('base.html', title='Reset Database')
-    
-    @app.route('/populate')
-    def populate():
-        # create users in the DB
-        add_or_update_user('Austen')
-        add_or_update_user('NASA')
-        add_or_update_user('Austen')
-        add_or_update_user('elonmusk')
 
-        return render_template('base.html', title='Populate Database')
-    
     @app.route('/update')
     def update():
         # get list of usernames of all users
         users = User.query.all()
-        
+
         for username in [user.username for user in users]:
             add_or_update_user(username)
 
         return render_template('base.html', title='Users Updated')
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<username>', methods=['GET'])
+    def user(username=None, message=''):
+        username = username or request.values['user_name']
+
+        try:
+            if request.method == 'POST':
+                add_or_update_user(username)
+                message = f'User "{username}" has been successfully added!'
+
+            posts = User.query.filter(User.username == username).one().posts
+
+        except Exception as e:
+            message = f'Error adding {username}: {e}'
+            posts = []
+
+        return render_template('user.html', title=username, posts=posts, message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user0, user1 = sorted([request.values['user0'], request.values['user1']])
+        hypo_post_text = request.values['post_text']
+
+        if user0 == user1:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user0, user1, hypo_post_text)
+
+            # get into the if statement if the prediction is user1
+            if prediction:
+                message = f'"{hypo_post_text}" is more likely to be said by {user1} than by {user0}'
+            else:
+                message = f'"{hypo_post_text}" is more likely to be said by {user0} than by {user1}'
+
+        return render_template('prediction.html', title='Prediction', message=message)
 
     return app
 
 
 nlp = spacy.load('my_model/')
 # we have the same tool we used in the flask shell
+
+
 def vectorize_post(post_text):
     '''
     Give the function some text
